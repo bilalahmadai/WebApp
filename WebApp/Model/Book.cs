185cf9c5b0af946d6aa08de09bb97a6ade966d48
bilalansar3 <bilalahmadai971@gmail.com>
2022-10-21 10:20:12 +0500

    model created

---
+++ WebApp/Model/Book.cs
@@ -0,0 +1,21 @@
+﻿using System.Text.Json.Serialization;
+using System.Text.Json;
+
+namespace WebApp.Model
+{
+    public class Book
+    {
+        [JsonPropertyName("id")]
+        public int booK_id { get; set; }
+        [JsonPropertyName("name")]
+        public string name { get; set; }
+        [JsonPropertyName("image")]
+        public string image { get; set; }
+
+        public override string ToString()
+        {
+            return JsonSerializer.Serialize<Book>(this);
+        }
+
+    }
+}
