e3353e815a8c3f90b52082116cf77a0cdc3b1f9e
bilalansar3 <bilalahmadai971@gmail.com>
2022-10-21 10:33:27 +0500

    index.cs and index.chtml updated

--- WebApp/Pages/Index.cshtml.cs
+++ WebApp/Pages/Index.cshtml.cs
@@ -5,21 +5,31 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WebApp.Model;
+using WebApp.Services;
 
 namespace WebApp.Pages
 {
     public class IndexModel : PageModel
     {
+        JsonFileBookService BookService;
+
+        // Need variable for Book Record
+        public IEnumerable<Book> Books;
+
+
         private readonly ILogger<IndexModel> _logger;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(ILogger<IndexModel> logger, JsonFileBookService services)
         {
             _logger = logger;
+            BookService = services;
+
         }
 
         public void OnGet()
         {
-
+            Books = BookService.getBookRecord();
         }
     }
 }
