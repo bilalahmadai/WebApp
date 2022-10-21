e161bce819c23818b9b9c0eb834888524cfcf453
bilalansar3 <bilalahmadai971@gmail.com>
2022-10-21 10:24:37 +0500

    service created

---
+++ WebApp/Services/JsonFileBookService.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+using WebApp.Model;
+
+namespace WebApp.Services
+{
+    public class JsonFileBookService
+    {
+
+        public IWebHostEnvironment WebHostEnvironment { get; set; }
+
+        public JsonFileBookService(IWebHostEnvironment webHostEnvironment)
+        {
+            WebHostEnvironment = webHostEnvironment;
+        }
+
+        public string fileAddress
+        {
+            get
+            {
+                return Path.Combine(WebHostEnvironment.WebRootPath, "data", "books.json");
+            }
+        }
+
+        public IEnumerable<Book> getBookRecord()
+        {
+            using (var filestream = File.OpenText(fileAddress))
+            {
+                return JsonSerializer.Deserialize<Book[]>(filestream.ReadToEnd());
+            }
+        }
+    }
+}
